c6320f3b2cd03d41d17e7120a94982e847d3e6c4
AkshayDhanbhar1001 <108855982+AkshayDhanbhar1001@users.noreply.github.com>
2022-07-07 12:14:15 +0530

    Add files via upload
    program interview on python

---
+++ interview question python.py
@@ -0,0 +1,6 @@
+# write a python program which accepts
+#Write a Pythonprogram which accepts the radius of a circle from the user and compute the area.
+#area=3.14*r^2
+r = float(input("enter the radius"))
+area = 3.14* r*r
+print("area if circle=",area)
