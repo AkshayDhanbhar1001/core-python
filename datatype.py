3206fb3765db3e447bfebbe3979a4765d81af04d
AkshayDhanbhar1001 <108855982+AkshayDhanbhar1001@users.noreply.github.com>
2022-07-07 11:26:56 +0530

    Day 3 task
    Number and type conversion

---
+++ datatype.py
@@ -0,0 +1,35 @@
+# type conversion
+a=10
+# check memory location
+print(id(a))
+# to check data type
+print(type(a))
+#to check data type
+print(bin(a))
+# integer to binary
+print(bin(a))
+#binary to integer
+print(int(0b01111))
+#----------------#
+#integer to float
+print(float(a))
+#integer to octal
+print(oct(a))
+#---------------#
+#octal to integer
+print(int(0o56))
+#--------------#
+#print int to hexadecimal
+print(hex(a))
+#hex to int
+print(int(0x32))
+#integer to complex
+print(complex(a))
+#to check real and img part
+num1=5+3j
+print(num1.real)
+print(num1.imag)
+
+
+      
+
