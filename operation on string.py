16b27a781b0b9eb1dd3c7d47a0180d2d48c04eed
AkshayDhanbhar1001 <108855982+AkshayDhanbhar1001@users.noreply.github.com>
2022-07-09 10:40:45 +0530

    operation on string

---
+++ operation on string.py
@@ -0,0 +1,24 @@
+#operations on string
+
+s1= 'yasham academy'
+
+#indexing(+ve
+print(s1[2])
+
+#-ve indexing
+print(s1[-14])
+
+#slicing(+ve)
+print(s1[0:6])
+
+#slicing with negative indexing
+print(s1[-14:-8])
+
+#step size(+ve)
+print(s1[0:13:2])
+
+#-ve step size
+print(s1[-14:-1:3])
+
+#reversing the string
+print(s1[ : :-1])
