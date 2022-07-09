1aec751e7b11c785900e32add267a6f8e153ea8e
AkshayDhanbhar1001 <108855982+AkshayDhanbhar1001@users.noreply.github.com>
2022-07-09 11:14:28 +0530

    concanation .py

---
+++ concatination.py
@@ -0,0 +1,19 @@
+#write to perform concatanation and repation on string
+#concatanation
+f_name = 'Akshay'
+m_name = 'Suresh'
+l_name = 'Dhanbhar'
+#method1
+print(f_name,m_name,l_name)
+
+#method2
+print(f_name+' '+m_name+' '+l_name)
+
+#method3
+print(f'hello,{l_name} {f_name} how are you ?')
+
+#method4
+print("hi{} {}, how are you?".format(f_name,l_name))
+
+#method5
+print(f_name*4)
